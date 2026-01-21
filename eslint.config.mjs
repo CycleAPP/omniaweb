e1eba28cab9f68febaaefa5922f7f96a2672e88b
ph43rn <ph43rn@localhost.localdomain>
2026-01-20 18:54:15 -0600

    Change port to 6432

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import globals from "globals";
+import nextPlugin from "@next/eslint-plugin-next";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default [
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+    settings: {
+      next: {
+        rootDir: ["./"],
+      },
+    },
+  },
+];
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-# Kill process on port 3000 if it exists
-fuser -k 5005/tcp || true
+# Kill process on port 6432 if it exists
+fuser -k 6432/tcp || true
 npm install
 npm run build
-npm start -- -p 5005
+npm start -- -p 6432
